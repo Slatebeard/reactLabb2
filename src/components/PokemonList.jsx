74416ddcb869ed7ffee868ccba31867be4ac0983
Slatebeard <max.ekberg@iths.se>
2025-09-06 13:54:43 +0200

    add pokemonlist.jsx but does not show list...FIX

---
+++ src/components/PokemonList.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+const PokemonList = () => {
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      const response = await fetch(
+        "https://pokeapi.co/api/v2/pokemon?limit=20"
+      );
+      const data = await response.json();
+      setPokemon(data.results);
+    };
+    fetchPokemon();
+  }, []);
+
+  return (
+    <div className="pokemonList">
+      <h2>Pokémon Lista</h2>
+      <ul>
+        {pokemon.map((mon) => {
+          const id = mon.url.split("/").slice(-2, -1)[0];
+          return (
+            <li key={id}>
+              <Link to={`/item/${id}`}>{mon.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default PokemonList;
